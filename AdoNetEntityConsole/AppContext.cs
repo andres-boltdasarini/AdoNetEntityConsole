2d65d63bffcda9525d8e2af330360fe3e1ddda61
Андрей <lau47@mail.ru>
2025-08-10 13:47:08 +0300

    Добавьте файлы проекта.

---
+++ AdoNetEntityConsole/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdoNetEntityConsole
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Database=ent;
+              Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
