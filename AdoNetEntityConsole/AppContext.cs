9139f05d634e6779e274b5b611ee7df3b17f7ab7
Андрей <lau47@mail.ru>
2025-08-10 14:23:46 +0300

    changeonpostgres

--- AdoNetEntityConsole/AppContext.cs
+++ AdoNetEntityConsole/AppContext.cs
@@ -1,26 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace AdoNetEntityConsole
 {
     public class AppContext : DbContext
     {
-        // Объекты таблицы Users
         public DbSet<User> Users { get; set; }
 
-        public AppContext()
-        {
-            Database.EnsureCreated();
-        }
+        public AppContext() => Database.EnsureCreated();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Database=ent;
-              Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=ent;Username=postgres;Password=1");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Основная таблица
+            modelBuilder.Entity<User>().ToTable("users");
+
+            // Автоматическое преобразование в нижний регистр
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.GetTableName().ToLower());
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(property.GetColumnName().ToLower());
+                }
+            }
         }
     }
 }
