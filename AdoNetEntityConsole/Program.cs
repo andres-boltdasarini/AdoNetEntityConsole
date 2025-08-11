d0764a887008dc9af14fd1b1410a2b6bd6b62494
Андрей <lau47@mail.ru>
2025-08-11 09:23:06 +0300

    2535_repo

--- AdoNetEntityConsole/Program.cs
+++ AdoNetEntityConsole/Program.cs
@@ -10,43 +10,30 @@ namespace ElectronicLibrary
         {
             using (var db = new AppContext())
             {
-                // Создаем тестовые данные
-                var user1 = new User { Name = "Иван Иванов", Email = "ivan@example.com" };
-                var user2 = new User { Name = "Мария Петрова", Email = "maria@example.com" };
+                // Создание репозиториев
+                var userRepo = new UserRepository(db);
+                var bookRepo = new BookRepository(db);
 
-                var book1 = new Book { Title = "Война и мир", PublicationYear = 1869 };
-                var book2 = new Book { Title = "Преступление и наказание", PublicationYear = 1866 };
-                var book3 = new Book { Title = "1984", PublicationYear = 1949 };
+                // Добавление пользователя
+                var newUser = new User { Name = "Алексей Сидоров", Email = "alex@example.com" };
+                userRepo.Add(newUser);
 
-                db.Users.AddRange(user1, user2);
-                db.Books.AddRange(book1, book2, book3);
-                db.SaveChanges();
+                // Обновление имени пользователя
+                userRepo.UpdateUserName(1, "Иван Иванович Иванов");
 
-                // Выдаем книги
-                var loan1 = new BookLoan { UserId = user1.Id, BookId = book1.Id };
-                var loan2 = new BookLoan { UserId = user1.Id, BookId = book2.Id };
-                var loan3 = new BookLoan { UserId = user2.Id, BookId = book3.Id };
+                // Добавление книги
+                var newBook = new Book { Title = "Мастер и Маргарита", PublicationYear = 1966 };
+                bookRepo.Add(newBook);
 
-                db.BookLoans.AddRange(loan1, loan2, loan3);
-                db.SaveChanges();
+                // Обновление года книги
+                bookRepo.UpdateBookYear(1, 1870);
 
-                // Возвращаем одну книгу
-                loan1.ReturnDate = DateTime.UtcNow;
-                db.SaveChanges();
+                // Получение книги по ID
+                var book = bookRepo.GetById(1);
+                Console.WriteLine($"Обновленная книга: {book.Title} ({book.PublicationYear})");
 
-                // Выводим информацию
-                Console.WriteLine("Пользователи и их книги:");
-                foreach (var user in db.Users.Include(u => u.BookLoans).ThenInclude(bl => bl.Book))
-                {
-                    Console.WriteLine($"{user.Name} ({user.Email}):");
-                    foreach (var loan in user.BookLoans)
-                    {
-                        Console.WriteLine($"- {loan.Book.Title} ({loan.Book.PublicationYear}) " +
-                                          $"[Выдана: {loan.LoanDate:d}, " +
-                                          $"{(loan.ReturnDate.HasValue ? "Возвращена: " + loan.ReturnDate.Value.ToString("d") : "На руках")}]");
-                    }
-                    Console.WriteLine();
-                }
+                // Удаление книги
+                bookRepo.Delete(bookRepo.GetById(3));
             }
         }
     }
---
+++ AdoNetEntityConsole/Repository.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicLibrary
+{
+    // Репозиторий для работы с пользователями
+    public class UserRepository
+    {
+        private readonly AppContext _context;
+
+        public UserRepository(AppContext context)
+        {
+            _context = context;
+        }
+
+        // Получение пользователя по ID
+        public User GetById(int id)
+        {
+            return _context.Users
+                .Include(u => u.BookLoans)
+                .ThenInclude(bl => bl.Book)
+                .FirstOrDefault(u => u.Id == id);
+        }
+
+        // Получение всех пользователей
+        public List<User> GetAll()
+        {
+            return _context.Users
+                .Include(u => u.BookLoans)
+                .ThenInclude(bl => bl.Book)
+                .ToList();
+        }
+
+        // Добавление пользователя
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        // Удаление пользователя
+        public void Delete(User user)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+
+        // Обновление имени пользователя
+        public void UpdateUserName(int id, string newName)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                user.Name = newName;
+                _context.SaveChanges();
+            }
+        }
+    }
+
+    // Репозиторий для работы с книгами
+    public class BookRepository
+    {
+        private readonly AppContext _context;
+
+        public BookRepository(AppContext context)
+        {
+            _context = context;
+        }
+
+        // Получение книги по ID
+        public Book GetById(int id)
+        {
+            return _context.Books
+                .Include(b => b.Loans)
+                .ThenInclude(bl => bl.User)
+                .FirstOrDefault(b => b.Id == id);
+        }
+
+        // Получение всех книг
+        public List<Book> GetAll()
+        {
+            return _context.Books
+                .Include(b => b.Loans)
+                .ThenInclude(bl => bl.User)
+                .ToList();
+        }
+
+        // Добавление книги
+        public void Add(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        // Удаление книги
+        public void Delete(Book book)
+        {
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+        }
+
+        // Обновление года выпуска книги
+        public void UpdateBookYear(int id, int newYear)
+        {
+            var book = _context.Books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+            {
+                book.PublicationYear = newYear;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
